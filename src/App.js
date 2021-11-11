54eda40ab86d2db9c3e83d23231676eda769ceb7
Harry <hdhillon478@gmail.com>
2021-11-11 16:31:19 +0000

    Build functions to iteratrate and fetch pokemon

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { getAllPokemon } from './services/Pokemon';
+import { getAllPokemon, getPokemon } from './services/Pokemon';
 import Navbar from './components/layout/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -15,15 +15,26 @@ function App() {
   const initialURL = 'https://pokeapi.co/api/v2/pokemon'
 
   useEffect(() => {
+    // Initial API call to get and set data in state
     async function fetchData() {
       let response = await getAllPokemon(initialURL)
-      console.log(response);
       setNextUrl(response.nest)
       setPrevUrl(response.prev)
+      // Response is the api response, results is the name of the array in the API object
+      await loadingPokemon(response.results)
       setLoading(false)
     }
     fetchData()
   }, [])
+
+  const loadingPokemon = async (data) => {
+    let _pokemonData = await Promise.all(data.map(async pokemon => {
+      let pokemonRecord = await getPokemon(pokemon.url)
+      return pokemonRecord
+    }))
+
+    setPokemonData(_pokemonData)
+  }
 
   return (
     <div className="App">
--- src/services/Pokemon.js
+++ src/services/Pokemon.js
@@ -8,3 +8,11 @@ export async function getAllPokemon(url) {
             })
     });
 }
+
+export async function getPokemon(url) {
+    return new Promise((resolve, reject) => {
+        fetch(url).then(res => res.json()).then(data => {
+            resolve(data)
+        })
+    })
+}
