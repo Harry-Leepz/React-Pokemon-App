124ba43de647b1de10210fc87bc877d6c5f42bf5
Harry <hdhillon478@gmail.com>
2021-11-11 14:02:52 +0000

    Create state cars and navbar styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React, { useState, useEffect } from 'react';
+import Navbar from './components/layout/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
+
+
 function App() {
+  // State Variables
+  const [pokemonData, setPokemonData] = useState([])
+  const [nextUrl, setNextUrl] = useState('');
+  const [prevUrl, setPrevUrl] = useState('');
+  const [loading, setLoading] = useState(true);
+  const initialURL = 'https://pokeapi.co/api/v2/pokemon'
+
+
   return (
     <div className="App">
-      <h1> Hello harry</h1>
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav className='navbar navbar-expand-md'>
+            <h4 className='text-light font-weight-bold'>Something Pokemon</h4>
+        </nav>
+    )
+}
+
+export default Navbar
