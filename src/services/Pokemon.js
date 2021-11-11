fc86e6bb65450ece79b9c3a7423d0645c47d793b
Harry <hdhillon478@gmail.com>
2021-11-11 14:16:32 +0000

    Add comments to getAllPokemon function

--- src/services/Pokemon.js
+++ src/services/Pokemon.js
@@ -1,3 +1,5 @@
+// Make APi call to PokeAPI to get a list of All pokemon.
+// https://pokeapi.co/api/v2/pokemon
 export async function getAllPokemon(url) {
     return new Promise((resolve, reject) => {
         fetch(url).then(res => res.json())
